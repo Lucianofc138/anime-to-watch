c089df7915124bfc7d5d658b5da9c1769025d377
Lucianofc138 <55565367+Lucianofc138@users.noreply.github.com>
2024-07-20 15:56:02 -0400

    fix(gh action): add url setting to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/anime-to-watch/', // This should match your repository name
+  plugins: [react()]
 })
